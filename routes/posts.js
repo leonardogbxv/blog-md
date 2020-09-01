dcee97e9ccf60a83ba2b3019dbd181d216beb0a5
Leonardo Gomes Batista <leonardog.bat@gmail.com>
2020-09-01 18:38:12 -0300

    Refactor '/edit' and '/new' routes code

--- routes/posts.js
+++ routes/posts.js
@@ -3,10 +3,16 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('./../models/post');
+const { findById } = require('./../models/post');
 
 // Relative to '/posts' route in server.js
 router.get('/new', (req, res) => {
   res.render('posts/new', { post: new Post() });
+});
+
+router.get('/edit/:id', async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  res.render('posts/edit', { post: post });
 });
 
 router.get('/:slug', async (req, res) => {
@@ -17,26 +23,39 @@ router.get('/:slug', async (req, res) => {
   res.render('posts/show', { post: post });
 });
 
-router.post('/', async (req, res) => {
-  let post = new Post({
-    title: req.body.title,
-    category: req.body.category,
-    description: req.body.description,
-    markdown: req.body.markdown
-  });
+// Post route to create a new blog 'post'
+router.post('/', async (req, res, next) => {
+  req.post = new Post();
+  next();
+}, savePost('new'));
 
-  try {
-    post = await post.save();
-    res.redirect(`/posts/${post.slug}`);
-  } catch(e) {
-    console.log(e);
-    res.render('posts/new', { post: post });
-  }
-});
+router.put('/:id', async (req, res, next) => {
+  req.post = await Post.findById(req.params.id);
+  next();
+}, savePost('edit'));
 
+// Delete blog 'post'
 router.delete('/:id', async (req, res) => {
   await Post.findByIdAndDelete(req.params.id);
   res.redirect('/');
 });
+
+// Save blog 'post' and redirect (middleware)
+function savePost(path) {
+  return async (req, res) => {
+    let post = req.post
+    post.title = req.body.title,
+    post.category = req.body.category,
+    post.description = req.body.description,
+    post.markdown = req.body.markdown
+    try {
+      post = await post.save();
+      res.redirect(`/posts/${post.slug}`);
+    } catch(e) {
+      console.log(e);
+      res.render(`posts/${path}`, { post: post });
+    }
+  }
+}
 
 module.exports = router;
