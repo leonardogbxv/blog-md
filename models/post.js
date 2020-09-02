50d6ce5e2de35db6a93ebe4ea70a53df4ab7c119
Leonardo Gomes Batista <leonardog.bat@gmail.com>
2020-09-02 12:57:52 -0300

    Add dompurify and JSDOM

--- models/post.js
+++ models/post.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 const marked = require('marked');
 const slugify = require('slugify');
 
+// Allows DOMPurify to create HTML and purify it with the JSDOM window object
+const createDOMPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+const DOMPurify = createDOMPurify(new JSDOM().window);
+
 const PostSchema = new mongoose.Schema({
   title: {
     type: String,
@@ -27,13 +32,22 @@ const PostSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true
+  },
+  sanitizedHTML: {
+    type: String,
+    required: true
   }
 });
 
-// slug validation
+// validations
 PostSchema.pre('validate', function(next) {
+  // create slug based on the 'post title'
   if(this.title) {
     this.slug = slugify(this.title, { lower: true, strict: true});
+  }
+  // converts the markdown to HTML and sanitize it
+  if(this.markdown) {
+    this.sanitizedHTML = DOMPurify.sanitize(marked(this.markdown));
   }
 
   next();
